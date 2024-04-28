78704c9661d57f41fdc75846ba905fbbe394a36a
kyle <kyletalin@gmail.com>
2024-04-28 18:25:50 -0400

    Changed icon and readme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { ModeToggle } from "@/components/mode-toggle"
-import { Github } from "lucide-react"
+import { Github, RefreshCw } from "lucide-react"
 import { Button } from "./ui/button"
 
 export function Header() {
   return (
     <div className="border h-16 flex justify-between items-center px-4">
-      Habit Loop
+      <div className="flex gap-2 items-center">
+        Habit Loop
+        <RefreshCw />
+      </div>
       <div className="flex items-center gap-2"> 
         <Button asChild variant='outline' size='icon'>
           <a href="https://github.com/pursuance/habit-loop" target="_blank">
