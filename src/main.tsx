f82a0f56ee6370af9805939ded1e75634ad2ef98
pursuance <kyletalin@gmail.com>
2024-05-06 15:52:46 -0400

    Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { 
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import { App } from '@/routes/App.tsx'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { CalendarHeatMap } from '@/components/CalendarHeatMap'
+import { HabitTable } from '@/components/HabitTable'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HabitTable />
+      },
+      {
+        path: 'habits/:habitId',
+        element: <CalendarHeatMap />
+      } 
+    ]
+  }
+  
+    
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/routes/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from '@/components/Header'
+import { Outlet } from 'react-router-dom'
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
+}
